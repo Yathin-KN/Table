c42f8174bf91717f5982a5003acf3d5899e20670
myselfshravan <myselfshravan@gmail.com>
2023-08-11 23:10:51 +0530

    vercelroutes and slice

--- store/slices/menuSlice.ts
+++ store/slices/menuSlice.ts
@@ -21,15 +21,18 @@ const menuSlice = createSlice({
         state[food_id].quantity_bought -= 1;
       }
     },
-    resetCartItems:(state)=>{
-      state=initialState
-    }
+    resetCartItems: (state) => {
+      Object.keys(state).forEach((food_id) => {
+        state[food_id].quantity_bought = 0;
+      });
+    },
   },
 });
 
-export const { increaseQuantity, decreaseQuantity , resetCartItems } = menuSlice.actions;
+export const { increaseQuantity, decreaseQuantity, resetCartItems } =
+  menuSlice.actions;
 
-export const selectQuantity = (state:any, food_id:any) =>
+export const selectQuantity = (state: any, food_id: any) =>
   state.menu[food_id] ? state.menu[food_id].quantity_bought : 0;
 
 export default menuSlice.reducer;
