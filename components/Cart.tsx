ffd9295d04cb0e54ac93d2703983ed27f84458c1
myselfshravan <myselfshravan@gmail.com>
2023-08-20 05:23:50 +0530

    import err

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { useDispatch, useSelector } from "react-redux";
-import React, { useState } from "react";
+import { useState } from "react";
 import {
   selectDishItems,
   clearItems as DishClear,
